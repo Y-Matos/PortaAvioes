8ab3d4d64fb94860a298e976c6e7c88ad75701b3
Yuri de Oliveira Matos <yurimatos40@gmail.com>
2019-04-01 21:33:32 -0300

    novo metodo utilizado

--- PortaAvioes.cpp
+++ PortaAvioes.cpp
@@ -15,5 +15,10 @@ PortaAvioes::~PortaAvioes()
 void PortaAvioes::defineTripulacaoInicial(int tripulacaoInicial){
     
     tripulacaoQuantidade = tripulacaoInicial;
-    std::cout << "A tripulacao e de : " << tripulacaoQuantidade << std::endl;
+    std::cout << "A tripulacao inicial e de : " << tripulacaoQuantidade << std::endl;
+}
+
+void PortaAvioes::adicionaTripulacao(int novaTripulacao){
+    tripulacaoQuantidade += novaTripulacao;
+    std::cout << "A nova tripulacao e de : " << tripulacaoQuantidade << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@
 int main(int argc, char **argv)
 {
 	PortaAvioes portaAviao1(50);
+    
+    portaAviao1.adicionaTripulacao(46);
 	return 0;
 }
